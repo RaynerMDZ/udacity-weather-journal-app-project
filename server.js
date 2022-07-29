8e9314d6e78d908f7a8159f518eb670383294977
raynermdz <raynermendezg@gmail.com>
2022-07-29 01:49:47 -0400

    Add boilerplate and configuraiton code to server.js

--- server.js
+++ server.js
@@ -2,18 +2,26 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross-origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
